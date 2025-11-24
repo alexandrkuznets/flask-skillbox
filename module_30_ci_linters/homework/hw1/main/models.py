c8adf5d394f22afaff1d35fcfe09a97e60f2f8da
Alex <162812165+alexandrkuznets@users.noreply.github.com>
2025-11-24 22:38:59 +0300

    Update models.py

--- module_30_ci_linters/homework/hw1/main/models.py
+++ module_30_ci_linters/homework/hw1/main/models.py
@@ -33,7 +33,9 @@ class Parking(db.Model):  # type: ignore[name-defined]
 class ClientParking(db.Model):  # type: ignore[name-defined]
     __tablename__ = "client_parking"
     __table_args__ = (
-        db.UniqueConstraint("client_id", "parking_id", name="unique_client_parking"),
+        db.UniqueConstraint(
+            "client_id", "parking_id", name="unique_client_parking"
+        ),
     )
 
     id = db.Column(db.Integer, primary_key=True)
